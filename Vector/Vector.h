65c25dfbae29a42ed7c9fbd26c5f8bf2bbb84c8b
凪 千本 <Senbon.Nagi@outlook.com>
2019-02-01 17:47:29 +0800

    update some functions

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -32,17 +32,18 @@ public:
 	// 析构并释放内存空间
 	~Vector() { delete[] _elem; }
 
-	T & operator[]( Rank index ) const;                      // Vector 寻秩访问
-	T & operator=( const Vector<T> & vec );                  // Vector 赋值克隆
+	T & operator[]( Rank index ) const;                        // Vector 寻秩访问
+	T & operator=( const Vector<T> & vec );                    // Vector 赋值克隆
 
-	Rank size() const { return _size; }                      // 规模
-	bool empty() const { return !_size; }                    // 判空
-	void insert( Rank index, const T & e );                  // 插入
-	bool remove( const T & val );                            // 移除元素 val
-	void remove( Rank lo, Rank hi );                         // 移出区间 [ lo, hi)
-	bool isordered( bool ascending = true ) const;           // 是否升序排列
-	Rank find( const T & e, Rank lo = 0, Rank hi = _size );  // 遍历查找元素 e
-
+	Rank size() const { return _size; }                        // 规模
+	bool empty() const { return !_size; }                      // 判空
+	void insert( Rank index, const T & val );                  // 插入元素 val
+	void remove( Rank lo, Rank hi );                           // 移出区间 [ lo, hi )
+	const T remove( Rank index );                              // 按秩移除元素
+	bool isordered( bool ascending = true ) const;             // 是否升序排列
+	Rank find( const T & val, Rank lo = 0, Rank hi = _size );  // 遍历查找元素 val
+	Rank findRemove( const T & val );                          // 移除元素 val
+	int  deduplicate();                                        // 无序 Vector 去重
 
 };
 
@@ -123,28 +124,13 @@ inline T & Vector<T>::operator=( const Vector<T> & vec ) {
 }
 
 template<typename T>
-inline void Vector<T>::insert( Rank index, const T & e ) {
+inline void Vector<T>::insert( Rank index, const T & val ) {
 	expend();
 	for ( int i = _size; i > index; i-- ) {
 		_elem[i] = _elem[i - 1];
 	}
-	_elem[index] = e;
+	_elem[index] = val;
 	++_size;
-}
-
-template<typename T>
-inline bool Vector<T>::remove( const T & val ) {
-	for ( int i = 0; i < _size; i++ ) {
-		if ( _elem == val ) {
-			int index = i + 1;
-			while ( index < _size ) {
-				_elem[index - 1] = _elem[index];
-			}
-			--_size;
-			return true;
-		}
-	}
-	return false;
 }
 
 template<typename T>
@@ -156,6 +142,13 @@ inline void Vector<T>::remove( Rank lo, Rank hi ) {
 	}
 	_size = lo;
 	shrink();
+}
+
+template<typename T>
+inline const T Vector<T>::remove( Rank index ) {
+	T val = _elem[index];
+	remove( index, index + 1 );
+	return val;
 }
 
 template<typename T>
@@ -177,13 +170,39 @@ inline bool Vector<T>::isordered( bool ascending ) const {
 }
 
 template<typename T>
-inline Rank Vector<T>::find( const T & e, Rank lo, Rank hi ) {
+inline Rank Vector<T>::find( const T & val, Rank lo, Rank hi ) {
 	for ( int i = lo; i < hi; i++ ) {
-		if ( e == _elem[i] )
+		if ( _elem[i] == val )
 			return i;
 	}
 	return -1;
 }
+
+template<typename T>
+inline Rank Vector<T>::findRemove( const T & val ) {
+	for ( int i = 0; i < _size; i++ ) {
+		if ( _elem == val ) {
+			int index = i + 1;
+			while ( index < _size ) {
+				_elem[index - 1] = _elem[index];
+			}
+			--_size;
+			return i;
+		}
+	}
+	return -1;
+}
+
+template<typename T>
+inline int Vector<T>::deduplicate() {
+	int oldSize = _size;
+	Rank i = 1;
+	while ( i < _size ) {
+		(find( _elem[i], 0, i ) > 0) ? remove( i ) : i++;
+	}
+	return oldSize - _size;
+}
+
 
 // swap function
 template<typename T>
