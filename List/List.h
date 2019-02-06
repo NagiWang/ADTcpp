49085484d15beccf5cc2c700909e379fae28e79e
凪 千本 <Senbon.Nagi@outlook.com>
2019-02-06 17:01:52 +0800

    update

--- List/List.h
+++ List/List.h
@@ -23,8 +23,8 @@ template<typename T>
 inline void List<T>::init() {
 	header = new ListNode<T>;
 	trailer = new ListNode<T>;
-	header->succ = trailer;
 	header->pred = nullptr;
+	header->succ = trailer;
 	trailer->pred = header;
 	trailer->succ = nullptr;
 	_size = 0;
