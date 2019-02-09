2885ba35049ba6dfde347a34bd87dbee8a56a2f2
凪 千本 <Senbon.Nagi@outlook.com>
2019-02-09 23:55:05 +0800

    update

--- List/List.h
+++ List/List.h
@@ -16,6 +16,9 @@ public:
 	~List() {}
 
 	Posi<T> first() const { return header->succ; }
+	Posi<T> last() const { return trailer->pred; }
+
+	T & operator[]( Rank index ) const;
 
 };
 
@@ -29,5 +32,13 @@ void List<T>::init() {
 	trailer->pred = header;
 	trailer->succ = nullptr;
 	_size = 0;
+}
+
+template<typename T>
+T & List<T>::operator[]( Rank index ) const {
+	Posi<T> p = first();
+	while ( index-- > 0 )
+		p = p->succ;
+	return p->data;
 }
 
