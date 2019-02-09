75f42352e7bc0023eebe3579a36951121c3421b2
凪 千本 <Senbon.Nagi@outlook.com>
2019-02-10 01:03:53 +0800

    update

--- List/List.h
+++ List/List.h
@@ -10,15 +10,33 @@ private:
 
 protected:
 	void init();
+	void copyNodes( Posi<T> p, Rank index );
 
 public:
 	List() { init(); }
+	List( const List<T> & L ) { copyNodes( L.first(), L._size ); }
+	List( const List<T> & L, Rank index, Rank n ) { copyNodes( L[index], n ); }
+	List( Posi<T> p, Rank n ) { copyNodes( p, n ); }
+
 	~List() {}
+
+	Rank size() const { return _size; }
+	bool empty() const { return _size <= 0; }
 
 	Posi<T> first() const { return header->succ; }
 	Posi<T> last() const { return trailer->pred; }
 
 	T & operator[]( Rank index ) const;
+
+	Posi<T> find( const T & val, Posi<T> p, Rank n );
+	Posi<T> find( const T & val );
+
+	Posi<T> inserAsFirst( const T & val );
+	Posi<T> inserAsLast( const T & val );
+	Posi<T> inserPred( Posi<T> p, const T & val );
+	Posi<T> inserSucc( Posi<T> p, const T & val );
+
+	const T remove( Posi<T> p );
 
 };
 
@@ -35,10 +53,68 @@ void List<T>::init() {
 }
 
 template<typename T>
+void List<T>::copyNodes( Posi<T> p, Rank index ) {
+	init();
+	while ( n-- ) {
+		inserAsLast( p->data );
+		p = p->succ;
+	}
+}
+
+
+template<typename T>
 T & List<T>::operator[]( Rank index ) const {
 	Posi<T> p = first();
-	while ( index-- > 0 )
+	while ( index-- )
 		p = p->succ;
 	return p->data;
+}
+
+template<typename T>
+Posi<T> List<T>::find( const T & val, Posi<T> p, Rank n ) {
+	while ( n-- ) {
+		if ( val == ( p = p->pred )->data )
+			return p;
+	}
+	return nullptr;
+}
+
+template<typename T>
+Posi<T> List<T>::find( const T & val ) {
+	return find( val, header, _size );
+}
+
+template<typename T>
+Posi<T> List<T>::inserAsFirst( const T & val ) {
+	_size++;
+	return header->insertAsSucc( val );
+}
+
+template<typename T>
+Posi<T> List<T>::inserAsLast( const T & val ) {
+	_size++;
+	return trailer->insertAsPred( val );
+}
+
+template<typename T>
+Posi<T> List<T>::inserPred( Posi<T> p, const T & val ) {
+	_size++;
+	return p->insertAsPred( val );
+}
+
+template<typename T>
+Posi<T> List<T>::inserSucc( Posi<T> p, const T & val ) {
+	_size++;
+	return p->insertAsSucc( val );
+}
+
+template<typename T>
+const T List<T>::remove( Posi<T> p ) {
+	T val = p.data;
+	p->pred->succ = p->succ;
+	p->succ->pred = p->pred;
+	delete p;
+	_size--;
+	return val;
 }
 
--- List/main.cpp
+++ List/main.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include "ListNode.h"
+#include "List.h"
 using namespace std;
 
 
@@ -10,4 +11,13 @@ int main() {
 	a->succ = new ListNode<int>( 6, a );
 	auto b = a->succ;
 	cout << b->insertAsPred( 7 )->data << endl;
+	delete a, b;
+	List<int> L;
+	for ( int i = 0; i < 10; i++ ) {
+		L.inserAsLast( i + 10 );
+	}
+	for ( int i = 0; i < 10; i++ ) {
+		cout << L[i] << endl;
+	}
+	return 0;
 }
