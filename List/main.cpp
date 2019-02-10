e98da6853805e2028fbad7cafb24b4bf8b708293
凪 千本 <Senbon.Nagi@outlook.com>
2019-02-11 00:00:54 +0800

    update

--- List/main.cpp
+++ List/main.cpp
@@ -12,12 +12,13 @@ int main() {
 	auto b = a->succ;
 	cout << b->insertAsPred( 7 )->data << endl;
 	delete a, b;
-	List<int> L;
+	List<int> * pL = new List<int>;
 	for ( int i = 0; i < 10; i++ ) {
-		L.inserAsLast( i + 10 );
+		( *pL ).inserAsLast( i + 10 );
 	}
 	for ( int i = 0; i < 10; i++ ) {
-		cout << L[i] << endl;
+		cout << ( *pL )[i] << endl;
 	}
+	delete pL;
 	return 0;
 }
