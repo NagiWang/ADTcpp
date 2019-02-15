0560a9097027534b97a3507df1bce9febb416eff
凪 千本 <Senbon.Nagi@outlook.com>
2019-02-15 23:52:36 +0800

    update

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
 	Stack_Vector<int> st;
 	for ( int i = 0; i < 10; i++ ) {
-		st.push( 10 - i );
+		st.push( i );
 	}
 	for ( int i = 0; i < 10; i++ ) {
 		cout << st.top() << endl;
