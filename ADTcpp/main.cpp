86ad2b9361e953cba0aa104705296b57c92ff0a8
凪 千本 <Senbon.Nagi@outlook.com>
2019-02-17 23:35:31 +0800

    uodate

---
+++ ADTcpp/main.cpp
@@ -0,0 +1,36 @@
+﻿#include "paren.h"
+#include <iostream>
+using namespace std;
+
+bool Paren( const std::string & str );
+
+int main() {
+	string str;
+	while ( getline( cin, str ) ) {
+		cout << Paren( str ) << endl;
+	}
+	return 0;
+}
+
+bool Paren( const std::string & str ) {
+	Stack<char> st;
+	for ( auto val : str ) {
+		switch ( val ) {
+			case '(': case '[': case '{':
+				st.push( val );
+				break;
+			case ')':
+				if ( st.empty() or ( '(' != st.pop() ) ) return false;
+				break;
+			case ']':
+				if ( st.empty() or ( '[' != st.pop() ) ) return false;
+				break;
+			case '}':
+				if ( st.empty() or ( '{' != st.pop() ) ) return false;
+				break;
+			default:
+				break;
+		}
+	}
+	return st.empty();
+}
---
+++ ADTcpp/paren.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <string>
+//#include "../Stack/Stack_Vector.h"
+//template<typename T> using Stack = Stack_Vector<T>;
+#include "../Stack/Stack_List.h"
+template<typename T> using Stack = Stack_List<T>;
+
+
+// 括号匹配
