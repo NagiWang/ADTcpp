89ce292482f11586e839e5f26f6b0f18a55efa9c
凪 千本 <Senbon.Nagi@outlook.com>
2019-02-25 00:07:37 +0800

    opdate

--- ADTcpp/compute.h
+++ ADTcpp/compute.h
@@ -1,10 +1,19 @@
 ﻿#pragma once
-
+#include "Operator.h"
+#include "../Stack/Stack_List.h"
+template<typename T> using Stack = Stack_List<T>;
+// #include "../Stack/Stack_Vector.h"
+// template<typename T> using Stack = Stack_Vector<T>;
 
 template<typename T>
-class compute {
-public:
-	compute();
-	~compute();
-};
+T compute( char * str, char * & RPN ) {
+	Stack<T> opnd;
+	Stack<char> optr;
+	optr.push( '\0' );
+	while ( !optr.empty() ) {
+		if ( isdigit( *str ) ) {
+
+		}
+	}
+}
 
