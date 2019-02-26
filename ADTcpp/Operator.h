e099263019de8aaa817a11e8bb09794e821fae95
凪 千本 <Senbon.Nagi@outlook.com>
2019-02-26 22:45:32 +0800

    update

--- ADTcpp/Operator.h
+++ ADTcpp/Operator.h
@@ -1,9 +1,36 @@
 ﻿#pragma once
+#include <string>
 
+const int N_OPTR = 9;
 
 enum class Operator :int { ADD, SUB, MUL, DIV, POW, FAC, L_P, R_P, EOP };
+struct OpString {
+	std::string opWord[N_OPTR] {
+		"ADD",
+		"SUB",
+		"MUL",
+		"DIV",
+		"POW",
+		"FAC",
+		"L_P",
+		"R_P",
+		"EOP"
+	};
+	std::string opSign[N_OPTR] {
+		" + ",
+		" - ",
+		" * ",
+		" / ",
+		"^",
+		"!",
+		"(",
+		")",
+		"EOP"
+	};
+};
+
 typedef Operator Op;
-const int N_OPTR = 9;
+typedef OpString Ops;
 
 const char Pri[N_OPTR][N_OPTR] =
 {
