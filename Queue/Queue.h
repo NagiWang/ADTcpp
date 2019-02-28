9ff847e4d87fbfbab6a19eb9afea41d50a5791ff
凪 千本 <Senbon.Nagi@outlook.com>
2019-02-28 08:55:55 +0800

    apply Queue class

---
+++ Queue/Queue.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "../List/List.h"
+
+
+template<typename T>
+class Queue : public List<T> {
+public:
+	void enqueue( const T & e ) { *this->inserAsLast( e ); }
+	T dequeue() { return remove( first() ); }
+	T & front() { return first()->data; }
+};
