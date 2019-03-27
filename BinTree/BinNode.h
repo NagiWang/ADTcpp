46af5888d4bd2b0b23f86ed10756b1539a5c4bfc
凪 千本 <Senbon.Nagi@outlook.com>
2019-03-28 00:08:46 +0800

    UPDATE

--- BinTree/BinNode.h
+++ BinTree/BinNode.h
@@ -1,13 +1,15 @@
 #pragma once
 
 template<typename T>
-struct BinNode;                             // 二叉树结点类
+struct BinNode;                      // 二叉树结点类
 
 template<typename T>
-using BinNodePosi = BinNode<template> *;    // 二叉树结点指针
+using BinNodePosi = BinNode<T> *;    // 二叉树结点指针
 
-template<typename T>
-constexpr auto stature( T p ) { return ( ( p ) ? ( p )->height : -1 ); } // 节点高度 ( 空树高度为-1 )
+template<typename T>                 // 节点高度 ( 空树高度为-1 )
+constexpr auto stature( T p ) {
+	return ( ( p ) ? ( p )->height : -1 );
+} 
 
 typedef enum { RB_RED, RB_BLACK } RBColor;  // 节点颜色
 
