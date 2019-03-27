733a753603740ca2d395e74b8e4bc752960afe8e
凪 千本 <Senbon.Nagi@outlook.com>
2019-03-27 23:10:43 +0800

    update

--- BinTree/BinNode.h
+++ BinTree/BinNode.h
@@ -3,5 +3,6 @@
 template<typename T> struct BinNode;                             // 二叉树结点类
 template<typename T> using BinNodePosi = BinNode<template> *;    // 二叉树结点指针
 
-
+template<typename T>
+constexpr auto stature( T p ) { return ( ( p ) ? ( p )->height : -1 ); }
 
