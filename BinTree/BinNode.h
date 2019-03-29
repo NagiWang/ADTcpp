2d0fc31d1a5070f122e29a1824014209ded83fed
凪 千本 <Senbon.Nagi@outlook.com>
2019-03-29 23:45:32 +0800

    update

--- BinTree/BinNode.h
+++ BinTree/BinNode.h
@@ -9,7 +9,7 @@ using BinNodePosi = BinNode<T> *;    // 二叉树结点指针
 template<typename T>                 // 节点高度 ( 空树高度为-1 )
 constexpr auto stature( T p ) {
 	return ( ( p ) ? ( p )->height : -1 );
-} 
+}
 
 typedef enum { RB_RED, RB_BLACK } RBColor;  // 节点颜色
 
@@ -31,6 +31,5 @@ template<typename T>struct BinNode {
 			 RBColor c = RB_RED ) :
 		data( e ), parent( p ), lc( lc ), rc( rc ),
 		height( h ), npl( l ), color( c ) {}
-
 
 };
